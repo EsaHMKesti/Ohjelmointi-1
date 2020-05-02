3b85285a365e85c967db70948a2c193be66be7a7
EsaHMKesti <63982745+EsaHMKesti@users.noreply.github.com>
2020-05-03 00:14:01 +0100

    Lisäys demoon
    Lisäys demoihin

---
+++ MuutujienKuvaamista.java
@@ -0,0 +1,35 @@
+package demot;
+
+/**
+ * @author esakesti
+ *Esitellään muuttujien nimeämistä ja arvoja
+ */
+public class Muuttujat {
+
+	/**
+	 * @param args ei käytössä
+	 */
+	public static void main(String[] args) {
+
+		double työmatkaPyörällä = 5;
+		double suoritetutOpintopisteet = 10;
+		double tutkinnonLaajuusOpintopisteissä = 120;
+		double tehdytDemot = 2;
+		char ensimmäinenKirjain = 'K';
+		double voittoAikaSadalla = 9.8;
+
+
+		System.out.println("Työmatka pyörällä = " + työmatkaPyörällä);
+		System.out.println("Suoritetut opintopisteet = " + suoritetutOpintopisteet);
+		System.out.println("Tutkinnon laajuus opintopisteissä = " + tutkinnonLaajuusOpintopisteissä);
+		System.out.println("Tehdyt Demot = " + tehdytDemot);
+		System.out.println("Kirjan ensimmäinen kirjain = " + ensimmäinenKirjain);
+		System.out.println("Sadan metrin juoksun voittajan aika = " + voittoAikaSadalla);
+
+
+
+
+
+
+		   }
+}
